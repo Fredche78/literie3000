4c5e574d0bb42d02a10626f749515a6f30d55cec
Frédéric <fchenneviere@gmail.com>
2022-04-05 17:29:00 +0200

    Ajout de la fonction suppression

--- index.php
+++ index.php
@@ -5,6 +5,17 @@ $query = $db->query("SELECT * FROM matelas");
 
 $matelas = $query->fetchAll();
 
+if (!empty($_POST)) {
+
+    $delete = trim(strip_tags($_POST["delete"]));
+    $query = $db->prepare("DELETE FROM matelas WHERE id= :delete");
+    $query->bindParam(":delete", $delete);
+
+    // if ($_GET["id"] === $item["id"]) {
+    ($query->execute());
+    // header("index.php");
+}
+
 include("templates/header.php")
 ?>
 <h1>Catalogue</h1>
@@ -30,12 +41,35 @@ include("templates/header.php")
                 <p id="promo"><?= $item["prix"] ?> €</p>
                 <p><?= $item["prix_promo"] ?> €</p>
             </div>
+            <div class="idProd">
+                <p>ID: <?= $item["id"] ?></p>
+                <p </p>
+            </div>
+
+            <!-- <div class="suppression">
+                <form action="" method="post">   
+                    <input class="btn-supprimer" name="delete" type="submit" value="Supprimer">
+                    <input type="text" name="delete" id="inputDelete" value="<?= isset($delete) ? $delete : "" ?>">
+                </form>
+                <a href="index.php?id="<?= $item["id"] ?>">Supprimer</a>
+            </div> -->
+
+            <!-- <input class="btn-supprimer" name="<?= $item["id"] ?>" type="submit" value="Supprimer"> -->
 
         </div>
     <?php
     }
     ?>
+
+    <div class="suppression">
+        <form action="index.php" method="post">   
+            <label for="inputDelete">Tapez l'ID du produit pour le supprimer</label>
+            <input type="text" name="delete" id="inputDelete" value="<?= isset($delete) ? $delete : "" ?>">
+        </form>
+    </div>
 </div>
+
+
 
 <div class="ajout">
     <a href="add_matelas.php">
